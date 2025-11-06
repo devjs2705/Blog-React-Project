801131a8ffbbd56eb28a2937f866d93ceed5ca1a
devjs2705 <devjs2705@gmail.com>
2025-11-06 22:58:26 +0530

    basic project setup done, env variables setup done

---
+++ react-blog-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import config from '../config/config'
+
+function App() {
+
+  console.log(config.appWriteBucketId)
+
+  return (
+    <>
+      <h1>Hello world!!!</h1>
+    </>
+  )
+}
+
+export default App
